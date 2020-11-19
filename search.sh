f18a0553c219a25d847b4bc51aa07a0236338d57
Aki Nakano <apollonia.an@gmail.com>
2020-11-19 22:18:04 +0900

    save

--- search.sh
+++ search.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# check effect of temp
-python main_cross_cs.py --label_smoothing 0.1 --uncertainty_weights --alpha 0.01 --gamma 0.01 -e 200 --scheduler_step_size 60 --temp 5 --run_only --notqdm
-python main_cross_cs.py --label_smoothing 0.1 --uncertainty_weights --alpha 0.05 --gamma 0.05 -e 200 --scheduler_step_size 60 --temp 5 --run_only --notqdm
-
-# check effect of batch normalization
-python main_cross_cs.py --label_smoothing 0.1 --uncertainty_weights --alpha 0.01 --gamma 0.01 -e 200 --scheduler_step_size 60 --run_only --notqdm --batch_norm
-
-# compare with other alpha/gamma
-python main_cross_cs.py --label_smoothing 0.1 --uncertainty_weights --alpha 0.1 --gamma 0.001 -e 200 --scheduler_step_size 60 --run_only --notqdm
+python main_cross_cs.py --label_smoothing 0.1 --uncertainty_weights --alpha 0.01 --gamma 0.01 -e 250 --scheduler_step_size 60 --run_only --notqdm
+python main_cross_cs.py --label_smoothing 0.1 --uncertainty_weights --alpha 0.01 --gamma 0.01 -e 250 --scheduler_step_size 60 --run_only --notqdm
