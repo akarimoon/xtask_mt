5f13bcfd9e966f14c0c5b2d91659d9940f893c32
akarimoon <apollonia.an@gmail.com>
2020-11-23 17:24:47 +0900

    save

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 #PBS -q h-regular
 #PBS -W group_list=po9025
 #PBS -l select=1:mpiprocs=4
-#PBS -l walltime=10:00:00
+#PBS -l walltime=12:00:00
 cd $PBS_O_WORKDIR
 source /lustre/po9025/o09025/.bashrc
 conda activate pytorch
 
-python main_cross_nyu.py --label_smoothing 0.1 --uncertainty_weights  -j 64 --run_only --notqdm --scheduler_step_size 90
+python main_cross_nyu.py --label_smoothing 0.1 --uncertainty_weights  -j 64 --run_only --notqdm --alpha 0.001 --gamma 0.001 
+python main_cross_nyu.py --label_smoothing 0.1 --uncertainty_weights  -j 64 --run_only --notqdm --alpha 0.01 --gamma 0.01
+python main_cross_nyu.py --label_smoothing 0.1 --uncertainty_weights  -j 64 --run_only --notqdm --alpha 0.1 --gamma 0.1
