2121051a6a398ebd304010bd75dd87904b595206
Aki Nakano <apollonia.an@gmail.com>
2020-12-06 16:04:09 +0900

    fix unpack issue

--- search.sh
+++ search.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-python main_cross_cs.py --label_smoothing 0.1 --uncertainty_weights --alpha 0.01 --gamma 0.01 -e 250 --scheduler_step_size 60 --run_only --notqdm
-python main_cross_cs.py --uncertainty_weights --alpha 0.01 --gamma 0.01 -e 250 --scheduler_step_size 60 --run_only --notqdm
-python main_cross_cs.py　--run_only --notqdm
+python main_cross_cs.py --label_smoothing 0.1 --uncertainty_weights --run_only --notqdm --wider_ttnet
+python main_cross_cs.py --uncertainty_weights --run_only --notqdm --batch_norm --wider_ttnet
