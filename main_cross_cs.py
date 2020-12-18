a486533f84ab263469d418928d430be93a728abb
Aki Nakano <apollonia.an@gmail.com>
2020-12-18 12:31:34 +0900

    save

--- main_cross_cs.py
+++ main_cross_cs.py
@@ -22,7 +22,9 @@ DEPTH_CORRECTION = 2.1116e-09
 def compute_loss(batch_X, batch_y_segmt, batch_y_depth, 
                  batch_mask_segmt, batch_mask_depth, 
                  model, task_weights=None,
-                 criterion=None, criterion2=None, optimizer=None, optimizer2=None, is_train=True, epoch=1):
+                 criterion=None, optimizer=None, 
+                 is_train=True):
+
     if task_weights is None:
         task_weights = [1, 1]
 
@@ -42,51 +44,76 @@ def compute_loss(batch_X, batch_y_segmt, batch_y_depth,
         optimizer.zero_grad()
         image_loss.backward(retain_graph=True)
         label_loss.backward(retain_graph=True)
+        optimizer.step()
 
-        if optimizer2 is not None:
-            alpha = 0.16
-            optimizer2.zero_grad()
+    return (task_weights[0] * image_loss).item() + (task_weights[1] * label_loss).item()
 
-            l1 = task_weights[0] * image_loss
-            l2 = task_weights[1] * label_loss
-            loss = torch.div(torch.add(l1,l2), 2)
+def compute_loss_with_gradnorm(batch_X, batch_y_segmt, batch_y_depth, 
+                               batch_mask_segmt, batch_mask_depth, 
+                               model, task_weights=None, l01=None, l02=None,
+                               criterion=None, criterion2=None, optimizer=None, optimizer2=None, 
+                               is_train=True, epoch=1):
 
-            if epoch == 1:
-                l01 = l1.data  
-                l02 = l2.data
-                
-            param = list(model.parameters())
-            G1R = torch.autograd.grad(l1, param[0], retain_graph=True, create_graph=True)
-            G1 = torch.norm(G1R[0], 2)
-            G2R = torch.autograd.grad(l2, param[0], retain_graph=True, create_graph=True)
-            G2 = torch.norm(G2R[0], 2)
-            G_avg = torch.div(torch.add(G1, G2), 2)
+    model.train(is_train)
+
+    batch_X = batch_X.to(device, non_blocking=True)
+    batch_y_segmt = batch_y_segmt.to(device, non_blocking=True)
+    batch_y_depth = batch_y_depth.to(device, non_blocking=True)
+    batch_mask_segmt = batch_mask_segmt.to(device, non_blocking=True)
+    batch_mask_depth = batch_mask_depth.to(device, non_blocking=True)
+
+    output = model(batch_X)
+    image_loss, label_loss = criterion(output, batch_y_segmt, batch_y_depth,
+                                       batch_mask_segmt, batch_mask_depth, task_weights=task_weights)
+
+    if is_train:
+
+        alpha = 0.16
+
+        l1 = task_weights[0] * image_loss * 0.5
+        l2 = task_weights[1] * label_loss * 0.5
+
+        if epoch == 1:
+            l01 = l1.data
+            l02 = l2.data
+
+        optimizer.zero_grad()
+        l1.backward(retain_graph=True)
+        l2.backward(retain_graph=True)
             
-            # Calculating relative losses 
-            lhat1 = torch.div(l1, l01)
-            lhat2 = torch.div(l2, l02)
-            lhat_avg = torch.div(torch.add(lhat1, lhat2), 2)
-            
-            # Calculating relative inverse training rates for tasks 
-            inv_rate1 = torch.div(lhat1, lhat_avg)
-            inv_rate2 = torch.div(lhat2, lhat_avg)
-            
-            # Calculating the constant target for Eq. 2 in the GradNorm paper
-            C1 = G_avg * inv_rate1 ** alpha
-            C2 = G_avg * inv_rate2 ** alpha
-            C1 = C1.detach()
-            C2 = C2.detach()
-            
-            # Calculating the gradient loss according to Eq. 2 in the GradNorm paper
-            Lgrad = torch.add(criterion2(G1, C1), criterion2(G2, C2))
-            Lgrad.backward()
-            
-            # Updating loss weights 
-            optimizer2.step()
+        param = list(model.parameters())
+        G1R = torch.autograd.grad(l1, param[0], retain_graph=True, create_graph=True)
+        G1 = torch.norm(G1R[0], 2)
+        G2R = torch.autograd.grad(l2, param[0], retain_graph=True, create_graph=True)
+        G2 = torch.norm(G2R[0], 2)
+        G_avg = (G1 + G2) / 2
+        
+        # Calculating relative losses 
+        lhat1 = torch.div(l1, l01)
+        lhat2 = torch.div(l2, l02)
+        lhat_avg = (lhat1 + lhat2) / 2
+        
+        # Calculating relative inverse training rates for tasks 
+        inv_rate1 = torch.div(lhat1, lhat_avg)
+        inv_rate2 = torch.div(lhat2, lhat_avg)
+        
+        # Calculating the constant target for Eq. 2 in the GradNorm paper
+        C1 = G_avg * inv_rate1 ** alpha
+        C2 = G_avg * inv_rate2 ** alpha
+        C1 = C1.detach()
+        C2 = C2.detach()
+        
+        optimizer2.zero_grad()
+        # Calculating the gradient loss according to Eq. 2 in the GradNorm paper
+        Lgrad = torch.add(criterion2(G1, C1), criterion2(G2, C2))
+        Lgrad.backward()
+        
+        # Updating loss weights 
+        optimizer2.step()
 
         optimizer.step()
 
-    return task_weights[0] * image_loss.item() + task_weights[1] * label_loss.item()
+    return (task_weights[0] * image_loss).item() + (task_weights[1] * label_loss).item(), l01, l02
 
 if __name__=='__main__':
     torch.manual_seed(0)
@@ -142,8 +169,6 @@ if __name__=='__main__':
     print("Options:")
     task_weights = None
     opt.balance_method = None
-    optimizer2 = None
-    criterion2 = None
     if opt.multiple_gpu:
         print("   multiple GPUs")
         model = torch.nn.DataParallel(model)
@@ -161,11 +186,12 @@ if __name__=='__main__':
     if opt.gradnorm:
         print("   use gradnorm")
         opt.balance_method = "gradnorm"
-        w_loss_1 = torch.ones((1,), requires_grad=True, device=device_name)
-        w_loss_2 = torch.ones((1,), requires_grad=True, device=device_name)
+        w_loss_1 = torch.tensor(1, requires_grad=True, dtype=torch.float32, device=device_name)
+        w_loss_2 = torch.tensor(1, requires_grad=True, dtype=torch.float32, device=device_name)
         task_weights = [w_loss_1, w_loss_2]
         optimizer2 = optim.Adam(task_weights, lr=opt.lr)
         criterion2 = nn.L1Loss()
+        l01, l02 = None, None
 
     print("Loading dataset...")
     train_data = CityscapesDataset(root_path=opt.input_path, height=opt.height, width=opt.width, num_classes=opt.num_classes,
@@ -182,7 +208,6 @@ if __name__=='__main__':
                           balance_method=opt.balance_method,
                           ignore_index=opt.ignore_index).to(device)
     optimizer = optim.Adam(parameters_to_train, lr=opt.lr, betas=opt.betas)
-    # optimizer = optim.SGD(parameters_to_train, lr=opt.lr)
     scheduler = optim.lr_scheduler.StepLR(optimizer, opt.scheduler_step_size, opt.scheduler_gamma)
 
     if not opt.infer_only:
@@ -201,20 +226,37 @@ if __name__=='__main__':
 
             for i, batch in enumerate(tqdm(train, disable=opt.notqdm)):
                 _, batch_X, batch_y_segmt, batch_y_depth, batch_mask_segmt, batch_mask_depth = batch
-                loss = compute_loss(batch_X, batch_y_segmt, batch_y_depth, 
-                                    batch_mask_segmt, batch_mask_depth, 
-                                    model, task_weights=task_weights,
-                                    criterion=criterion, criterion2=criterion2, optimizer=optimizer, optimizer2=optimizer2, 
-                                    is_train=True, epoch=epoch)
+                if not opt.gradnorm:
+                    loss = compute_loss(batch_X, batch_y_segmt, batch_y_depth, 
+                                        batch_mask_segmt, batch_mask_depth, 
+                                        model, task_weights=task_weights,
+                                        criterion=criterion, optimizer=optimizer,
+                                        is_train=True)
+                else:
+                    loss, l01, l02 = compute_loss_with_gradnorm(batch_X, batch_y_segmt, batch_y_depth, 
+                                        batch_mask_segmt, batch_mask_depth, 
+                                        model, task_weights=task_weights, l01=l01, l02=l02,
+                                        criterion=criterion, criterion2=criterion2, optimizer=optimizer, optimizer2=optimizer2,
+                                        is_train=True, epoch=epoch)
+                    coef = 2 / (w_loss_1 + w_loss_2)
+                    task_weights = [coef * w_loss_1, coef * w_loss_2]
+
                 train_loss += loss
 
             for i, batch in enumerate(tqdm(valid, disable=opt.notqdm)):
                 _, batch_X, batch_y_segmt, batch_y_depth, batch_mask_segmt, batch_mask_depth = batch
-                loss = compute_loss(batch_X, batch_y_segmt, batch_y_depth, 
-                                    batch_mask_segmt, batch_mask_depth, 
-                                    model, task_weights=task_weights,
-                                    criterion=criterion, criterion2=criterion2, optimizer=optimizer, optimizer2=optimizer2, 
-                                    is_train=False, epoch=epoch)
+                if not opt.gradnorm:
+                    loss = compute_loss(batch_X, batch_y_segmt, batch_y_depth, 
+                                        batch_mask_segmt, batch_mask_depth, 
+                                        model, task_weights=task_weights,
+                                        criterion=criterion, optimizer=optimizer,
+                                        is_train=False)
+                else:
+                    loss, l01, l02 = compute_loss_with_gradnorm(batch_X, batch_y_segmt, batch_y_depth, 
+                                        batch_mask_segmt, batch_mask_depth, 
+                                        model, task_weights=task_weights, l01=l01, l02=l02,
+                                        criterion=criterion, criterion2=criterion2, optimizer=optimizer, optimizer2=optimizer2,
+                                        is_train=False, epoch=epoch)
                 valid_loss += loss
 
             train_loss /= len(train.dataset)
@@ -231,8 +273,6 @@ if __name__=='__main__':
                 print("Uncertainty weights: segmt={:.5f}, depth={:.5f}".format(
                         (torch.exp(task_weights[1]) ** 0.5).item(), (torch.exp(task_weights[0]) ** 0.5).item()))
             if opt.gradnorm:
-                coef = 2 / torch.add(w_loss_1, w_loss_2)
-                params = [coef * w_loss_1, coef * w_loss_2]
                 print("GradNorm task weights: segmt={:.5f}, depth={:.5f}".format(
                         task_weights[1].item(), task_weights[0].item()))
 
@@ -271,7 +311,7 @@ if __name__=='__main__':
             model.module.load_state_dict(torch.load(weights_path, map_location=device))
 
     logger = Logger(num_classes=opt.num_classes, ignore_index=opt.ignore_index)
-    best_loss = 1e5
+    best_score = 0
     best_set = {}
 
     model.eval()
@@ -290,15 +330,16 @@ if __name__=='__main__':
 
             pred_segmt, pred_t_segmt, pred_depth, pred_t_depth = predicted
 
-            preds = [pred_segmt, pred_depth]
+            preds = [pred_t_segmt, pred_t_depth]
             targets = [batch_y_segmt, batch_y_depth]
             masks = [batch_mask_segmt, batch_mask_depth]
             logger.log(preds, targets, masks)
 
             # use best results for final plot
-            loss = overall_score(preds, targets)
-            if i == 0 or loss < best_loss:
-                best_set["loss"] = loss
+            score = overall_score(preds, targets)
+            if i == 0 or score > best_score:
+                best_score = score
+                best_set["score"] = score
                 best_set["original"] = original
                 best_set["targ_segmt"] = batch_y_segmt
                 best_set["targ_depth"] = batch_y_depth
