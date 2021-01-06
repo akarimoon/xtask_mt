ae830fb0bf5765057128ee04fbe9378deccdb4ed
Aki Nakano <apollonia.an@gmail.com>
2021-01-06 13:09:42 +0900

    feat: base stl net

--- model/xtask_ts.py
+++ model/xtask_ts.py
@@ -87,7 +87,7 @@ class DecoderSequential(nn.Module):
 
 class XTaskTSNet(nn.Module):
     def __init__(self, enc_layers, out_features_segmt=19, out_features_depth=1, 
-                 decoder_in_features=256, decoder_mid_features=128,
+                 decoder_mid_features=128,
                  batch_norm=False, use_pretrain=False, wider_ttnet=False):
         super(XTaskTSNet, self).__init__()
 
@@ -137,7 +137,7 @@ class XTaskTSNet(nn.Module):
         pretrained.layer4 = backbone.layer4
         return pretrained
 
-    def forward(self, x, infer_only=False):
+    def forward(self, x, direct_only=False):
         enc0 = self.pretrained_encoder.layer0(x)
         enc1 = self.pretrained_encoder.layer1(enc0)
         enc2 = self.pretrained_encoder.layer2(enc1)
@@ -158,10 +158,63 @@ class XTaskTSNet(nn.Module):
         dep0 = self.decoder_depth.conv5(torch.cat((dep1, enc0), dim=1))
         dep_out = self.decoder_depth.conv6(dep0)
 
-        if infer_only:
+        if direct_only:
             return seg_out, None, dep_out, None
 
         dep_tout = self.trans_s2d(seg_out)
         seg_tout = self.trans_d2s(dep_out)
 
         return seg_out, seg_tout, dep_out, dep_tout
+
+class BaseSTLNet(nn.Module):
+    def __init__(self, enc_layers, out_features, decoder_in_features=256, decoder_mid_features=128):
+        super(XTaskTSNet, self).__init__()
+
+        if enc_layers not in [18, 34, 50, 101, 152]:
+            raise ValueError("{} is not a valid number of resnet layers".format(enc_layers))
+
+        self.pretrained_encoder = self._load_encoder(enc_layers)
+        enc_features = np.array([64, 64, 128, 256, 512])
+        if enc_layers > 34:
+            enc_features[1:] *= 4
+
+        self.decoder = DecoderSequential(enc_features=enc_features, 
+                                         out_features=out_features, 
+                                         mid_features=decoder_mid_features)
+
+    def _load_encoder(self, enc_layers):
+        try:
+            backbone = torch.hub.load('pytorch/vision:v0.6.0', 'resnet' + str(enc_layers), pretrained=False)
+        except:
+            backbone = models.resnet34(pretrained=False)
+
+        pretrained = nn.Module()
+        pretrained.layer0 = nn.Sequential(
+                                backbone.conv1,
+                                backbone.bn1,
+                                backbone.relu                    
+                            )
+        pretrained.layer1 = nn.Sequential(
+                                backbone.maxpool,
+                                backbone.layer1
+                            )
+        pretrained.layer2 = backbone.layer2
+        pretrained.layer3 = backbone.layer3
+        pretrained.layer4 = backbone.layer4
+        return pretrained
+
+    def forward(self, x):
+        enc0 = self.pretrained_encoder.layer0(x)
+        enc1 = self.pretrained_encoder.layer1(enc0)
+        enc2 = self.pretrained_encoder.layer2(enc1)
+        enc3 = self.pretrained_encoder.layer3(enc2)
+        enc4 = self.pretrained_encoder.layer4(enc3)
+
+        out4 = self.decoder_segmt.conv1(enc4)
+        out3 = self.decoder_segmt.conv2(torch.cat((out4, enc3), dim=1))
+        out2 = self.decoder_segmt.conv3(torch.cat((out3, enc2), dim=1))
+        out1 = self.decoder_segmt.conv4(torch.cat((out2, enc1), dim=1))
+        out0 = self.decoder_segmt.conv5(torch.cat((out1, enc0), dim=1))
+        out = self.decoder_segmt.conv6(out0)
+
+        return out
