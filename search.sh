0a1c59d65a73acc221c19c8197356f4212f713cb
Aki Nakano <apollonia.an@gmail.com>
2021-01-13 20:33:30 +0900

    save

--- search.sh
+++ search.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python const_energy.py -e 100 --use_xtc
-python const_energy.py -e 100
+python const_energy.py -e 100 --use_xtc --notqdm
+python const_energy.py -e 100 --notqdm
 python const_energy.py --infer_only
